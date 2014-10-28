2c2a5fe7e877bca96e1d84b51e8a975d70ef0074
Paulsheldrake <Paulsheldrake@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-28 01:07:20 +0000

    tagging 1.2.3
    git-svn-id: https://plugins.svn.wordpress.org/mp3-to-post/tags/1.2.3@1015032 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mp3-to-post.php
+++ mp3-to-post.php
@@ -4,7 +4,7 @@
   Plugin URI: http://www.fractured-state.com/2011/09/mp3-to-post-plugin/
   Description: Creates posts using ID3 information in MP3 files.
   Author: Paul Sheldrake
-  Version: 1.1.0
+  Version: 1.2.3
   Author URI: http://www.fractured-state.com
  */
 
@@ -22,13 +22,13 @@ $mp3ToPostOptions = array(
   'folder_name' => $mp3FolderName,
   'folder_path' => $folderPath,
   'base_url_path' => $base_path,
- );
+);
 update_option('mp3-to-post', serialize($mp3ToPostOptions));
-  
+
 
 /* create the menu item and link to to an admin function */
 function mp3_admin_actions() {
-  add_options_page("MP3 to Post", "MP3 to Post", 1, "mp3-to-post", "mp3_admin");
+  add_options_page(__('MP3 to Post','mp3-to-post'), __('MP3 to Post','mp3-to-post'), 1, "mp3-to-post", "mp3_admin");
 }
 
 /* add the menu item */
@@ -36,7 +36,7 @@ add_action('admin_menu', 'mp3_admin_actions');
 
 /**
  * Creates the admin page for the plugin
- * 
+ *
  */
 function mp3_admin() {
   /**
@@ -44,44 +44,41 @@ function mp3_admin() {
    * http://wordpress.org/support/topic/plugin-blubrry-powerpress-podcasting-plugin-conflict-with-mp3-to-post-plugin?replies=1#post-2833002
    */
   require_once('getid3/getid3.php');
-  
+
   ?>
   <div class="wrap">
     <h2>MP3 to Post</h2>
     <?php
-			// load our variables in to an array
-      $mp3ToPostOptions = unserialize(get_option('mp3-to-post'));
+    // load our variables in to an array
+    $mp3ToPostOptions = unserialize(get_option('mp3-to-post'));
     ?>
-    <p><?php _e('This plugin will scan for MP3 files in the directory below and then add 
-      them as posts.  It takes the ID3v2 title and comment and sets it as the 
-      post title and content respectively.  It also takes the file and attaches 
-      it to the post and adds a link to the post content.'); ?></p>
-    <p><?php _e('The way the ID3 information is processed, <strong>the file needs to have the title and comment set in v1 and v2</strong>'); ?></p>
-    <p><?php _e('If the genre is set on the file, that will be turned in to the category. If more than one genre is set in the ID3 information MP3 to Post only takes the first one.  If the genre is not set the category on the post is set to the default option.'); ?></p>
+    <p><?php _e('This plugin will scan for MP3 files in the directory below and then add them as posts.  It takes the ID3v2 title and comment and sets it as the post title and content respectively.  It also takes the file and attaches it to the post and adds a link to the post content.','mp3-to-post'); ?></p>
+    <p><?php _e('The way the ID3 information is processed, <strong>the file needs to have the title and comment set in v1 and v2</strong>','mp3-to-post'); ?></p>
+    <p><?php _e('If the genre is set on the file, that will be turned in to the category. If more than one genre is set in the ID3 information MP3 to Post only takes the first one.  If the genre is not set the category on the post is set to the default option.','mp3-to-post'); ?></p>
     <?php create_folder($mp3ToPostOptions['folder_path']); ?>
-    <p><?php _e('Upload your files here:'); ?>
-      <?php echo $mp3ToPostOptions['base_url_path'] . '/' . 
-      $mp3ToPostOptions['folder_name']; ?> </p>
+    <p><?php _e('Upload your files here:','mp3-to-post'); ?>
+      <?php echo $mp3ToPostOptions['base_url_path'] . '/' .
+        $mp3ToPostOptions['folder_name']; ?> </p>
     <form method="post" action="">
-      <input type="submit" class="button-primary" name="create-all-posts" value="<?php _e('Create All Posts') ?>" />
-      <input type="submit" class="button-primary" name="create-first-post" value="<?php _e('Create 1st Post') ?>" />
+      <input type="submit" class="button-primary" name="create-all-posts" value="<?php _e('Create All Posts','mp3-to-post') ?>" />
+      <input type="submit" class="button-primary" name="create-first-post" value="<?php _e('Create 1st Post','mp3-to-post') ?>" />
     </form>
-    <?php      
-      // create some posts already!
-      if (isset($_POST['create-all-posts'])) {
-        echo '<pre>';
-        print_r(mp3_to_post('all', $mp3ToPostOptions['folder_path']));
-        echo '</pre>';
-      }
-      if (isset($_POST['create-first-post'])) {
-        echo '<pre>';
-        print_r(mp3_to_post(1, $mp3ToPostOptions['folder_path']));
-        echo '</pre>';
-      }
-      // end POST check
+    <?php
+    // create some posts already!
+    if (isset($_POST['create-all-posts'])) {
+      echo '<pre>';
+      print_r(mp3_to_post('all', $mp3ToPostOptions['folder_path']));
+      echo '</pre>';
+    }
+    if (isset($_POST['create-first-post'])) {
+      echo '<pre>';
+      print_r(mp3_to_post(1, $mp3ToPostOptions['folder_path']));
+      echo '</pre>';
+    }
+    // end POST check
     ?>
     <hr />
-    <h3><?php _e('Files listed in the order they will be added'); ?></h3>
+    <h3><?php _e('Files listed in the order they will be added','mp3-to-post'); ?></h3>
     <ol>
       <?php
       // get files
@@ -93,9 +90,9 @@ function mp3_admin() {
         echo '<li>
           <strong>' . $file . '</strong>
             <ul>
-              <li><strong>Title:</strong> '.$id3Details['title'].'</li>
-              <li><strong>Category:</strong> '.$id3Details['category'].'</li>
-              <li><strong>Comments:</strong> '.$id3Details['comment'].'</li>
+              <li><strong>' . _e('Title:', 'mp3-to-post') . '</strong> '.$id3Details['title'].'</li>
+              <li><strong>' . _e('Category:', 'mp3-to-post') . '</strong> '.$id3Details['category'].'</li>
+              <li><strong>' . _e('Comment:', 'mp3-to-post') . '</strong> '.$id3Details['comment'].'</li>
             </ul>
         </li>';
       }
@@ -103,7 +100,7 @@ function mp3_admin() {
     </ol>
 
   </div>
-  <?php     
+<?php
 }
 // end mp3_admin
 
@@ -144,28 +141,28 @@ function mp3_only($filename) {
 
 /**
  * Creates a post from an mp3 file.
- * 
+ *
  * @param $limit
  *  Limits the number of items created at one time.  Use an intager
- * 
+ *
  * @param $path
  *  The base path to the folder containing the mp3s to convert to posts
- * 
+ *
  * @return $array
  *   Will provide an array of messages
  */
 function mp3_to_post($limit = 'all', $folderPath) {
   $messages = array();
-  
+
   // get an array of mp3 files
   $mp3Files = mp3_array($folderPath);
 
   // check of there are files to process
   if(count($mp3Files) == 0){
-    array_push($messages, 'There are no files to process');
+    array_push($messages, _e('There are no files to process', 'mp3-to-post'));
     return $messages;
   }
- 
+
   // Initialize getID3 engine
   $getID3 = new getID3;
 
@@ -202,7 +199,7 @@ function mp3_to_post($limit = 'all', $folderPath) {
       $titleSearchResult = new WP_Query($searchArgs);
 
       // If there are no posts with the title of the mp3 then make the post
-      if ($titleSearchResult->post_count == 0) {    
+      if ($titleSearchResult->post_count == 0) {
         // create basic post with info from ID3 details
         $my_post = array(
           'post_title' => $title,
@@ -212,15 +209,15 @@ function mp3_to_post($limit = 'all', $folderPath) {
         );
         // Insert the post!!
         $postID = wp_insert_post($my_post);
-        
+
         // If the category/genre is set then update the post
-        if(!empty($category)){      
+        if(!empty($category)){
           $category_ID = get_cat_ID($category);
           // if a category exists 
           if($category_ID) {
             $categories_array = array($category_ID);
             wp_set_post_categories($postID, $categories_array);
-          } 
+          }
           // if it doesn't exist then create a new category
           else {
             $new_category_ID = wp_create_category($category);
@@ -228,7 +225,7 @@ function mp3_to_post($limit = 'all', $folderPath) {
             wp_set_post_categories($postID, $categories_array);
           }
         }
-        
+
         // move the file to the right month/date directory in wordpress
         $wpFileInfo = wp_upload_bits(basename($filePath), null, file_get_contents($filePath));
         // if moved correctly delete the original
@@ -239,10 +236,10 @@ function mp3_to_post($limit = 'all', $folderPath) {
         // add the mp3 file to the post as an attachment
         $wp_filetype = wp_check_filetype(basename($wpFileInfo['file']), null);
         $attachment = array(
-        'post_mime_type' => $wp_filetype['type'],
-         'post_title' => preg_replace('/\.[^.]+$/', '', basename($wpFileInfo['file'])),
-         'post_content' => '',
-         'post_status' => 'inherit'
+          'post_mime_type' => $wp_filetype['type'],
+          'post_title' => preg_replace('/\.[^.]+$/', '', basename($wpFileInfo['file'])),
+          'post_content' => '',
+          'post_status' => 'inherit'
         );
         $attach_id = wp_insert_attachment($attachment, $wpFileInfo['file'], $postID);
 
@@ -261,23 +258,23 @@ function mp3_to_post($limit = 'all', $folderPath) {
         wp_update_post($updated_post);
 
         // 
-        array_push($messages, 'Post created: ' . $title);
+        array_push($messages, _e('Post created:', 'mp3-to-post') . ' ' . $title);
       } else {
-        array_push($messages, 'Post already exists: ' . $title);
+        array_push($messages, _e('Post already exists:', 'mp3-to-post') . ' ' . $title);
       }
     } else {
-      array_push($messages, 'Either the title or comments are not set in the ID3 information.   Make sure they are both set for v1 and v2.');
+      array_push($messages, _e('Either the title or comments are not set in the ID3 information.   Make sure they are both set for v1 and v2.', 'mp3-to-post'));
     }
     $i++;
   endwhile; //
-  
+
   // return the messages
   return $messages;
 }
 
 /**
  * Creates a folder based on the path provided
- * 
+ *
  * @param $folderpath
  */
 function create_folder($folderPath){
@@ -288,12 +285,12 @@ function create_folder($folderPath){
     }
   }
 }
-    
+
 /**
  * Gives an array of mp3 files to turn in to posts
- * 
+ *
  * @param $folderPath
- * 
+ *
  * @return $array
  *  Returns an array of mp3 file names from the directory created by the plugin
  */
@@ -304,41 +301,41 @@ function mp3_array($folderPath){
   $mp3Files = array_filter($mp3Files, "mp3_only");
   // sort the files
   sort($mp3Files);
-  
+
   return $mp3Files;
 }
 
 
 /**
  * Gets the ID3 info of a file
- * 
+ *
  * @param $filePath
  * String, base path to the mp3 file
- * 
+ *
  * @return array
- * Keyed array with title, comment and category as keys.  
+ * Keyed array with title, comment and category as keys.
  */
 function get_ID3($filePath) {
-   // Initialize getID3 engine
+  // Initialize getID3 engine
   $get_ID3 = new getID3;
   $ThisFileInfo = $get_ID3->analyze($filePath);
 
   /**
    * Optional: copies data from all subarrays of [tags] into [comments] so
    * metadata is all available in one location for all tag formats
-   * metainformation is always available under [tags] even if this is not called 
+   * metainformation is always available under [tags] even if this is not called
    */
   getid3_lib::CopyTagsToComments($ThisFileInfo);
   $title = $ThisFileInfo['tags']['id3v2']['title'][0];
   $comment = $ThisFileInfo['tags']['id3v2']['comments'][0];
-  $category = $ThisFileInfo['tags']['id3v2']['genre'][0];  
-  
+  $category = $ThisFileInfo['tags']['id3v2']['genre'][0];
+
   $details = array(
     'title' => $title,
     'comment' => $comment,
     'category' => $category,
   );
-  
+
   return $details;
 }
 ?>
